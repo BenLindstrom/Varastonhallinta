6483edae904fd67109b2fdfcaae80fd942cceabb
JoonasPitk <46161@edu.raseko.fi>
2022-05-03 13:03:17 +0300

    Tuo loput sivut.

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -14,4 +14,38 @@ def index(request):
 def lainaus(request):
     return render(request, 'borrowing/lainaus.html')
 
+@login_required
+def palautus(request):
+    return render(request, 'return/palautus.html')
 
+@login_required
+def uusituote(request):
+    return render(request, 'newproduct/uusituote.html')
+
+@login_required
+def tuotelista(request):
+    return render(request, 'productlist/tuotelista.html')
+
+@login_required
+def lainantarkistus(request):
+    return render(request, 'borrowingcheck/lainantarkistus.html')
+
+@login_required
+def eipaasyoikeutta(request):
+    return render(request, 'noaccess/eipaasyoikeutta.html')
+
+@login_required
+def yllapito_etusivu(request):
+    return render(request, 'maintenance/yllapito_etusivu.html')
+
+@login_required
+def yllapito_korjaalaina(request):
+    return render(request, 'maintenance/yllapito_korjaalaina.html')
+
+@login_required
+def yllapito_korjaatuote(request):
+    return render(request, 'maintenance/yllapito_korjaatuote.html')
+
+@login_required
+def yllapito_poistatuote(request):
+    return render(request, 'maintenance/yllapito_poistatuote.html')
